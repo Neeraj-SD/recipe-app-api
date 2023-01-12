0e26c8d27191ddf539b8cfde2e862132b2f0ed6e
Neeraj S D <neerajsd04@gmail.com>
2023-01-13 00:19:27 +0530

    Recipe image api

--- app/core/models.py
+++ app/core/models.py
@@ -6,7 +6,16 @@ from django.contrib.auth.models import (
 )
 from django.conf import settings
 
-# Create your models here.
+import uuid
+import os
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image."""
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{(ext)}'
+
+    return os.path.join('uploads', 'recipe', filename)
 
 class UserManager(BaseUserManager):
     """Manager for users."""
@@ -57,12 +66,26 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
+    ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self) -> str:
         return self.title
 
 class Tag(models.Model):
     """Tags for filtering recipes."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredient for recipes."""
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,13 +5,18 @@ from django.shortcuts import render
 from rest_framework import (
     viewsets,
     mixins,
+    status,
     )
+
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from core.models import (
     Recipe,
-    Tag
+    Tag,
+    Ingredient,
     )
 from recipe import serializers
 
@@ -27,6 +32,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Return the serializer class for request."""
         if self.action == 'list':
             return serializers.RecipeSerializer
+
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
         
         return self.serializer_class
 
@@ -37,6 +45,17 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self,  request, pk=None):
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class TagViewSet(mixins.UpdateModelMixin,
                 mixins.ListModelMixin,
@@ -50,6 +69,20 @@ class TagViewSet(mixins.UpdateModelMixin,
 
     def get_queryset(self):
         """filter queryset to authenticated users."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(mixins.UpdateModelMixin,
+                            mixins.ListModelMixin,
+                            viewsets.GenericViewSet):
+    """Manage ingredients in the database."""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter querset to authenticated users."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
     
