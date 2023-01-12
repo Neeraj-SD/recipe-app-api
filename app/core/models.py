feec16e62213b7cbe14d242122c8d7e24d89da08
Neeraj S D <neerajsd04@gmail.com>
2023-01-12 22:45:37 +0530

    build tags endpoint

--- app/core/models.py
+++ app/core/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin,
 )
+from django.conf import settings
 
 # Create your models here.
 
@@ -43,3 +44,30 @@ class User(AbstractBaseUser,PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+class Recipe(models.Model):
+    """Recipe Model."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+         on_delete=models.CASCADE
+         )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self) -> str:
+        return self.title
+
+class Tag(models.Model):
+    """Tags for filtering recipes."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self) -> str:
+        return self.name
