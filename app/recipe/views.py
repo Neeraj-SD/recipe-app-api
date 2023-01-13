37135d689badc154caacb51f4eb63a8b0887acf1
Neeraj S D <neerajsd04@gmail.com>
2023-01-13 11:07:07 +0530

    recipe view 500 error fix

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -48,7 +48,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrieve recipes for authenticated users."""
         tags = self.request.query_params.get('tags')
-        ingredients = self.request.query_params('ingredients')
+        ingredients = self.request.query_params.get('ingredients')
         return self.queryset.filter(user=self.request.user).order_by('-id')
 
     def perform_create(self, serializer):
